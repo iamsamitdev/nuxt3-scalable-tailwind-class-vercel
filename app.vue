2b6b831fd7ea3c7c2e44ba3880cb3e669dca2e71
Samit Koyom <samitkoyom@gmail.com>
2023-01-28 18:05:26 +0700

    initial commit

---
+++ app.vue
@@ -0,0 +1,34 @@
+<script lang="ts" setup>
+  useHead({
+    title: 'Home',
+    meta: [
+      { 
+        name: 'description', 
+        content: 'Home Nuxt 3, IT Genius Engineering' 
+      },
+      {
+        name: 'keywords',
+        content: 'Home, Nuxt 3, Learning Nuxt 3'
+      },
+    ],
+  })
+</script>
+
+<!-- <template>
+  <div class="flex justify-center items-center h-screen">
+    <h1 class="text-3xl font-bold text-purple-600 animate-bounce">
+      <Icon name="icon-park:home-two"></Icon> Hello Nuxt 3
+    </h1>
+  </div>
+</template> -->
+
+<template>
+
+  <Head>
+    <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans+Thai:wght@300;400;500&display=swap" rel="stylesheet">
+  </Head>
+  
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,52 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+
+    app: {
+        head: {
+            htmlAttrs: {
+                lang: 'en'
+            },
+            bodyAttrs: {
+                class: 'demo'
+            },
+            charset: 'utf-8',
+            titleTemplate: '%s | IT Genius Engineering',
+            meta: [
+                {
+                    name: 'author',
+                    content: 'IT Genius Engineering Ltd., Thailand'
+                },
+                {
+                    name: 'viewport',
+                    content: 'width=device-width, initial-scale=1, maximum-scale=5'
+                }
+            ]
+        }
+    },
+
+    css: ['~/assets/css/tailwind.css'],
+    postcss: {
+        plugins: {
+            tailwindcss: {},
+            autoprefixer: {},
+        },
+    },
+
+    modules: [
+        'nuxt-icon',
+        // [
+        //     '@nuxtjs/robots',
+        //     {
+        //     UserAgent: "*",
+        //     Disallow: "",
+        //     }
+        // ],
+    ],
+
+    runtimeConfig: {
+        public: {
+            wpUri: process.env.WP_URI,
+        },
+    },
+
+})
